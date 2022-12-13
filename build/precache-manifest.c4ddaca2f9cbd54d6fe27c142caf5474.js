b5eec71c91fcc5987485d527d5e2daa34f360a45
emanuel Ocampo <ocampoemanuel192165@gmail.com>
2022-12-13 22:53:15 +0800

    updating text

---
+++ build/precache-manifest.c4ddaca2f9cbd54d6fe27c142caf5474.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb59f01c14de0c47a2b4122925343d6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbdeedb3774b8db58089",
+    "url": "/static/js/2.470170de.chunk.js"
+  },
+  {
+    "revision": "1e7e48612d6ae4359e97",
+    "url": "/static/js/main.5c1fb251.chunk.js"
+  },
+  {
+    "revision": "2535e277f6499596031f",
+    "url": "/static/js/runtime-main.9c1af6e4.js"
+  },
+  {
+    "revision": "adf3f01c6e0f34cc6b2ca19759a55588",
+    "url": "/static/media/logo.adf3f01c.svg"
+  }
+]);
